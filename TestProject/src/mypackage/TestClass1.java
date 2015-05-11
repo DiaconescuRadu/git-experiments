273f73793e3fcff3c4d67b498648768d3498ac6b
Diaconescu Radu <rDiaconescu@axway.com>
2015-05-11 15:43:11 +0300

    Adding some new code, this is the new text, amended.

--- TestProject/src/mypackage/TestClass1.java
+++ TestProject/src/mypackage/TestClass1.java
@@ -4,6 +4,10 @@ public class TestClass1 {
 	
 	public static void main(String args[]) {
 		System.out.println("Printing some stuff");
+		System.out.println("Adding some new code");
+		System.out.println("Just changing the previos commit");
+		
+
 	}
 
 }
